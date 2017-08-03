ace5c0467c11fc8ee71c24b1938a5da1ad390570
Akash Kava <support@neurospeech.com>
2017-08-03 22:48:20 +0530

    Added AtomDevice

--- UIAtoms/AtomDevice.cs
+++ UIAtoms/AtomDevice.cs
@@ -1,230 +1,138 @@
-﻿using NeuroSpeech.UIAtoms;
-using System;
-using System.Collections.Concurrent;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-[assembly: Xamarin.Forms.Dependency(typeof(AtomDevice))]
-
 namespace NeuroSpeech.UIAtoms
 {
+
     /// <summary>
     /// 
     /// </summary>
-    public class AtomDevice
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public AtomDevice()
-        {
-
-        }
-
-        private static AtomDevice _Device;
+    public class AtomDevice {
 
         /// <summary>
         /// 
         /// </summary>
-        public static AtomDevice Instance
-        {
-            get
-            {
-                return _Device ?? (_Device = DependencyService.Get<AtomDevice>());
-            }
-        }
+        public static AtomDevice Instance = new AtomDevice();
 
 
         /// <summary>
-        /// Executes asynchronous task on UI Thread, this is helpful when catching an exception
+        /// 
+        /// </summary>
+        public event EventHandler<LogEventArgs> LogEvent;
+
+
+        /// <summary>
+        /// 
         /// </summary>
         /// <param name="task"></param>
-        public static void RunOnUiThreadAsync(Func<Task> task) {
-            Instance.InvokeRunOnUiThreadAsync(task);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="ex"></param>
-        public static void ReportException(Exception ex) {
-            Instance.InternalReportException(ex);
-        }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="action"></param>
-        /// <param name="timeout"></param>
-        public static void Trigger(ref CancellationTokenSource id, Func<Task> action, TimeSpan timeout)
-        {
-            id = Instance.InternalTrigger(id, action, timeout);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="action"></param>
-        /// <param name="timeout"></param>
-        /// <returns></returns>
-        protected virtual CancellationTokenSource InternalTrigger(CancellationTokenSource id, Func<Task> action, TimeSpan timeout)
-        {
-            id?.Cancel();
-            id = UIAtomsApplication.Instance.SetTimeout(() =>
-            {
-                this.InvokeRunOnUiThreadAsync(action);
-            }, timeout);
-            return id;
-        }
-
-        private static TimeSpan defaultTimeout = TimeSpan.FromMilliseconds(100);
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="action"></param>
-        public static void Trigger(ref CancellationTokenSource id, Func<Task> action)
-        {
-            Trigger(ref id, action, defaultTimeout);
-        }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="action"></param>
-        public static void TriggerOnce(Func<Task> action)
-        {
-            TriggerOnce(action, defaultTimeout);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="action"></param>
-        /// <param name="timeout"></param>
-        public static void TriggerOnce(Func<Task> action, TimeSpan timeout)
-        {
-            Instance.InternalTriggerOnce(action, timeout);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="action"></param>
-        /// <param name="timeout"></param>
-        protected virtual void InternalTriggerOnce(Func<Task> action, TimeSpan timeout)
-        {
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                ActionKey key = new ActionKey(action);
-                CancellationTokenSource ct = null;
-                ct = UIAtomsApplication.Instance.SetTimeout(async () =>
-                {
-                    timeouts.TryRemove(key, out ct);
-                    try
-                    {
-                        await action();
+        /// <param name="logCancelledException"></param>
+        public void RunOnUIThread(Func<Task> task, bool logCancelledException = false) {
+            Device.BeginInvokeOnMainThread(async () => {
+                try {
+                    await task();
+                } catch (Exception ex) {
+                    if (ex is TaskCanceledException) {
+                        if (!logCancelledException)
+                            return;
                     }
-                    catch (Exception ex) {
-                        InternalReportException(ex);
-                    }
-                }, timeout);
-
-                timeouts.AddOrUpdate(key, ct, (k,oldValue) => {
-                    oldValue.Cancel();
-                    return ct;
-                });
-
-                ct.Token.Register(() => {
-                    timeouts.TryRemove(key, out ct);
-                });
-
+                    Log(ex);
+                }
             });
-
         }
 
 
-        //Dictionary<ActionKey, ActionKey> executionScope = new Dictionary<ActionKey, ActionKey>();
-        //public void TriggerNonRecursive(Action action) {
-        //    ActionKey key = new ActionKey(action);
-        //    if (executionScope.ContainsKey(key))
-        //        return;
-        //    try {
-        //        executionScope[key] = key;
-        //        action();
-        //    } finally {
-        //        executionScope.Remove(key);
-        //    }
-
-        //}
-
-        ConcurrentDictionary<ActionKey, CancellationTokenSource> timeouts = new ConcurrentDictionary<ActionKey, CancellationTokenSource>();
-
-        internal class ActionKey
-        {
-            private Func<Task> action;
-
-            public ActionKey(Func<Task> action)
-            {
-                this.action = action;
-            }
-
-            public override int GetHashCode()
-            {
-
-                string key = (action.Target?.GetType()?.FullName ?? "") + "." + (action.Method.ToString());
-                return key.GetHashCode();
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj == null)
-                    return false;
-                ActionKey k = obj as ActionKey;
-                if (k == null)
-                    return false;
-                if (k.action.Method != action.Method || k.action.Target != action.Target)
-                    return false;
-                return true;
-            }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="delay"></param>
+        /// <returns></returns>
+        public CancellationTokenSource SetTimeout(Func<Task> task, TimeSpan delay) {
+            return UIAtomsApplication.Instance.SetTimeout(() => RunOnUIThread(task), delay);
         }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="task"></param>
-        protected virtual void InvokeRunOnUiThreadAsync(Func<Task> task)
-        {
+        /// <param name="maxDelay"></param>
+        public void TriggerOnce(Func<Task> task, TimeSpan maxDelay) {
             Device.BeginInvokeOnMainThread(async () => {
                 try
                 {
-                    await task();
+                    var key = task.Method;
+                    CancellationTokenSource ct = null;
+                    if (timeouts.TryGetValue(key, out ct)) {
+                        ct.Cancel();
+                    }
+                    timeouts[key] = new CancellationTokenSource();
+                    try
+                    {
+                        await Task.Delay(maxDelay, ct.Token);
+                        if (!ct.IsCancellationRequested)
+                        {
+                            await task();
+                        }
+                    }
+                    catch (TaskCanceledException)
+                    {
+                    }
+                    catch (Exception ex)
+                    {
+                        Log(ex);
+                    }
+                    finally {
+                        timeouts.Remove(key);
+                    }
+                }
+                catch (TaskCanceledException) {
                 }
                 catch (Exception ex)
                 {
-                    InternalReportException(ex);
+                    Log(ex);
                 }
             });
-
         }
+
+        private Dictionary<System.Reflection.MethodInfo, CancellationTokenSource> timeouts = new Dictionary<System.Reflection.MethodInfo, CancellationTokenSource>();
+
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="ex"></param>
-        protected virtual void InternalReportException(Exception ex)
+        public void Log(Exception ex) {
+            LogEvent?.Invoke(this, new LogEventArgs { Error = ex });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Log(string msg)
         {
-            System.Diagnostics.Debug.Fail(ex.Message, ex.ToString());
+            LogEvent?.Invoke(this, new LogEventArgs { Text = msg });
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LogEventArgs : EventArgs
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Exception Error { get; set; }
+    }
+
 }
