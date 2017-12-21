3844bd74363cc7030c75352c276443ad8a169a7c
Hitesh Patil <hitesh.neurospeech@gmail.com>
2017-12-21 15:10:38 +0530

    Rotation done in ios

--- NeuroSpeech.UIAtoms.iOS/Drawing/AtomImageProvider.cs
+++ NeuroSpeech.UIAtoms.iOS/Drawing/AtomImageProvider.cs
@@ -45,27 +45,20 @@ namespace NeuroSpeech.UIAtoms.Drawing
             var image = await LoadAsync(source);
             return await Task.Run<string>(async () =>
             {
-                //var sourceSize = image.Size;
-                //UIGraphics.BeginImageContextWithOptions(new CGSize(sourceSize.Height, sourceSize.Width), true, 1.0f);
-                //CGContext bitmap = UIGraphics.GetCurrentContext();
-                //bitmap.DrawImage(new CGRect(0, 0, sourceSize.Height, sourceSize.Width), image.CGImage);
-                //bitmap.RotateCTM((float)(side.Equals("Left") ? Math.PI / 2 : -Math.PI / 2));
-                
-
-                //var r = new CGRect(0, 0, image.Size.Height, image.Size.Width);
-                //var cgi = image.CGImage.WithImageInRect(r);
-                //var bitmap1= new UIImage(cgi);
-
-
                 string name = System.IO.Path.GetFileNameWithoutExtension(source);
                 string ext = System.IO.Path.GetExtension(source);
                 var tempFile = $"{System.IO.Path.GetTempPath()}/{name}-{(Guid.NewGuid().ToString().Trim('{', '}'))}{ext}";
 
-                var newImage = ScaleImage(image,1);
+                var original = image;
 
                 var imageRotation = side.Equals("Left") ? UIImageOrientation.Left : UIImageOrientation.Right;
-                image  = UIImage.FromImage(image.CGImage, image.CurrentScale, imageRotation);
+                image = UIImage.FromImage(original.CGImage, original.CurrentScale, imageRotation);
+                var rect = new CGRect(0, 0, original.Size.Height, original.Size.Width);
+                UIGraphics.BeginImageContextWithOptions(rect.Size, true, 1.0f);
 
+                image.Draw(rect);
+                image = UIGraphics.GetImageFromCurrentImageContext();
+                UIGraphics.EndImageContext();
                 using (var s = System.IO.File.OpenWrite(tempFile))
                 {
                     var iss = image.AsPNG().AsStream();
@@ -103,7 +96,8 @@ namespace NeuroSpeech.UIAtoms.Drawing
             var data = await WebFetchAsync(img);
 
             var s = data as System.IO.Stream;
-            if (s != null) {
+            if (s != null)
+            {
                 return UIImage.LoadFromData(NSData.FromStream(s));
             }
 
@@ -119,78 +113,7 @@ namespace NeuroSpeech.UIAtoms.Drawing
             throw new NotSupportedException();
         }
 
-        public static UIImage ScaleImage(UIImage image, int maxSize)
-        {
-
-            UIImage res;
-
-            using (CGImage imageRef = image.CGImage)
-            {
-                CGImageAlphaInfo alphaInfo = imageRef.AlphaInfo;
-                CGColorSpace colorSpaceInfo = CGColorSpace.CreateDeviceRGB();
-                if (alphaInfo == CGImageAlphaInfo.None)
-                {
-                    alphaInfo = CGImageAlphaInfo.NoneSkipLast;
-                }
-
-                int width, height;
-
-                width = Convert.ToInt32(imageRef.Width);
-                height = Convert.ToInt32(imageRef.Height);
-
-
-                if (height >= width)
-                {
-                    width = (int)Math.Floor((double)width * ((double)maxSize / (double)height));
-                    height = maxSize;
-                }
-                else
-                {
-                    height = (int)Math.Floor((double)height * ((double)maxSize / (double)width));
-                    width = maxSize;
-                }
-
-
-                CGBitmapContext bitmap;
-
-                if (image.Orientation == UIImageOrientation.Up || image.Orientation == UIImageOrientation.Down)
-                {
-                    bitmap = new CGBitmapContext(IntPtr.Zero, width, height, imageRef.BitsPerComponent, imageRef.BytesPerRow, colorSpaceInfo, alphaInfo);
-                }
-                else
-                {
-                    bitmap = new CGBitmapContext(IntPtr.Zero, height, width, imageRef.BitsPerComponent, imageRef.BytesPerRow, colorSpaceInfo, alphaInfo);
-                }
-
-                switch (image.Orientation)
-                {
-                    case UIImageOrientation.Left:
-                        bitmap.RotateCTM((float)Math.PI / 2);
-                        bitmap.TranslateCTM(0, -height);
-                        break;
-                    case UIImageOrientation.Right:
-                        bitmap.RotateCTM(-((float)Math.PI / 2));
-                        bitmap.TranslateCTM(-width, 0);
-                        break;
-                    case UIImageOrientation.Up:
-                        break;
-                    case UIImageOrientation.Down:
-                        bitmap.TranslateCTM(width, height);
-                        bitmap.RotateCTM(-(float)Math.PI);
-                        break;
-                }
-
-                bitmap.DrawImage(new CGRect(0, 0, width, height), imageRef);
-
-
-                res = UIImage.FromImage(bitmap.ToImage());
-                bitmap = null;
-
-            }
-
-
-            return res;
-        }
 
     }
+
 }
